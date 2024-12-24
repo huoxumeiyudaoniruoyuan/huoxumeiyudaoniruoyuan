0e99533756dec2a736a15c2d30dc0da37c6173e2
kanbujianshini <132219170+huoxumeiyudaoniruoyuan@users.noreply.github.com>
2024-12-24 22:14:21 +0800

    创建 TV.js

---
+++ XPTV/TV.js
@@ -0,0 +1,17 @@
+{
+  "name": "xptv",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "axios": "^1.7.8",
+    "cheerio": "^1.0.0"
+  }
+}
